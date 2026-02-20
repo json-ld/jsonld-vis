3ae6f954cf03dd39c9d348f4b980a22ce9d16f90
Benjamin Young <byoung@digitalbazaar.com>
2026-02-20 15:17:43 -0500

    Upgrade d3 to v7; refactor to accomodate.
    The enter/update/exit pattern had heavily changed between d3 v3.5 and v7.
    Copilot (using GPT-5 mini) rewrote the code to use `link.join()` instead.
    
    The tree now displays immediately (because the proper data is now being
    used), but the alignment on clicking is still incorrect.

--- example/index.js
+++ example/index.js
@@ -1,10 +1,8 @@
-import d3 from 'd3';
+import * as d3 from 'd3';
 import jsonldVis from '../src/';
 import data from './example.json';
 
-jsonldVis(d3);
-
-d3.jsonldVis(data, '#graph', {
+jsonldVis(d3, data, '#graph', {
   w: 800,
   h: 600,
   maxLabelWidth: 250,
